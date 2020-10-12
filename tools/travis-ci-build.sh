d9fa0eebc3d093af07b690be8985e5cb7d1caeb0
Chuck Lu <chuck.lu@qq.com>
2020-10-12 18:48:23 +0800

    configure the travis ci

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,10 +2,10 @@
 echo "Changing to /src directory..."
 cd src
 echo "Executing MSBuild DLL begin command..."
-dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"chuckfork" /k:"ChuckFork_SuperSocket" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
-dotnet build SonarCloud.Travis.Integration.sln
+dotnet build ../SuperSocket.sln
 echo "Running tests..."
-dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj --logger:trx
+dotnet test ../test/SuperSocket.Tests/SuperSocket.Tests.csproj --logger:trx
 echo "Executing MSBuild DLL end command..."
 dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
